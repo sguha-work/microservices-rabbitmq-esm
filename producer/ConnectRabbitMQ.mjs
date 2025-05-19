5a2d1f4899a8588719255b456c70f64731d9e075
sguha-work <sguha1988.life@gmail.com>
2025-05-19 11:17:24 +0530

    comments added

--- producer/ConnectRabbitMQ.mjs
+++ producer/ConnectRabbitMQ.mjs
@@ -11,29 +11,29 @@ class ConnectRabbitMQ {
         return this;
     }
     async connect(maxRetry = 20) {
-        return new Promise((resolve, reject)=>{
-let index = 1;
-        const interval = setInterval(async () => {
-            try {
-                console.log(`Connecting to ${this.RABBITMQ_URL} and ${this.QUEUE_NAME}`);
-                console.log(`Attempt ${index}`);
-                index++;
-                const connection = await amqp.connect(this.RABBITMQ_URL);
-                const channel = await connection.createChannel();
-                await channel.assertQueue(this.QUEUE_NAME);
-                console.log(`Connected`);
-                clearInterval(interval);
-                resolve(channel);
-            } catch (error) {
-                if (index > maxRetry) {
-                    console.error('Max retry attempts reached. Exiting...');
+        return new Promise((resolve, reject) => {
+            let index = 1;
+            const interval = setInterval(async () => {
+                try {
+                    console.log(`Connecting to ${this.RABBITMQ_URL} and ${this.QUEUE_NAME}`);
+                    console.log(`Attempt ${index}`);
+                    index++;
+                    const connection = await amqp.connect(this.RABBITMQ_URL);
+                    const channel = await connection.createChannel();
+                    await channel.assertQueue(this.QUEUE_NAME);
+                    console.log(`Connected to ${this.RABBITMQ_URL} and ${this.QUEUE_NAME}`);
                     clearInterval(interval);
-                    reject(error);
-                } else {
-                    console.log('Previous attempt failed, retrying connection to RabbitMQ...');
+                    resolve(channel);
+                } catch (error) {
+                    if (index > maxRetry) {
+                        console.error('Max retry attempts reached. Exiting...');
+                        clearInterval(interval);
+                        reject(error);
+                    } else {
+                        console.log('Previous attempt failed, retrying connection to RabbitMQ...');
+                    }
                 }
-            }
-        }, 5000);
+            }, 5000);
         })
     }
 }
