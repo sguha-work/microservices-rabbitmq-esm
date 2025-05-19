dec8d18dc6a74fa4fa8cb81e88306b66a4b2c9c9
sguha-work <sguha1988.life@gmail.com>
2025-05-19 12:33:47 +0530

    2 different queue is used to implement duplex message

--- consumer/app.js
+++ consumer/app.js
@@ -2,36 +2,57 @@ import express from 'express';
 import ConnectRabbitMQ from './ConnectRabbitMQ.mjs';
 (() => {
   const app = express();
+  app.use(express.json());
   const PORT = process.env.PORT || 3001;
   const RABBITMQ_URL = `amqp://${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`;
-  const PRODUCER_QUEUE_NAME = 'producer_messages';
-  const connectRabbitMQ = new ConnectRabbitMQ();
+  const PRODUCER_QUEUE_NAME = 'producer_messages'; // will be used to consume message from producer
+  const CONSUMER_QUEUE_NAME = 'consumer_messages';// will be used to send message from consumer to producer
+  let consumerChannel1 = null;
+  let consumerChannel2 = null;
 
-  const startConsumer = async () => {
-    let channel = null;
-    try {
-      channel = await connectRabbitMQ.setRabbitMQUrl(RABBITMQ_URL).setQueueName(PRODUCER_QUEUE_NAME).connect();
-      console.log('Consumer waiting for messages...');
-    } catch (error) {
-      console.error('Error connecting to RabbitMQ:', error);
-      process.exit(1);
-    }
-    channel.consume(PRODUCER_QUEUE_NAME, async (message) => {
+
+  const initiateConsumingMessages = async () => {
+    consumerChannel1.consume(CONSUMER_QUEUE_NAME, async (message) => {
       if (message) {
         try {
           const content = JSON.parse(message.content.toString());
-          console.log(`Received message: ${content.text} at ${content.timestamp}`);
-          await channel.ack(message);
+          console.log(`Received message from producer: ${content.text} at ${content.timestamp}`);
+          await consumerChannel1.ack(message);
         } catch (error) {
           console.error('Error processing message:', error);
-          await channel.nack(message);
+          await consumerChannel1.nack(message);
         }
       }
     });
   };
 
-  app.listen(PORT, () => {
+  app.post('/consumer/send', async (req, res) => {
+    try {
+      if (!consumerChannel2) throw new Error('Not connected to RabbitMQ');
+      const message = {
+        text: req.body.text || 'Hello from consumer!',
+        timestamp: new Date().toISOString()
+      };
+      await consumerChannel2.sendToQueue(CONSUMER_QUEUE_NAME, Buffer.from(JSON.stringify(message)));
+      res.status(201).send((JSON.stringify({ data: 'Message sent' })));
+    } catch (error) {
+      console.error('Error sending message:', error);
+      res.status(500).send(error.message);
+    }
+  });
+
+  app.listen(PORT, async () => {
     console.log(`Consumer service running on port ${PORT}`);
-    startConsumer();
+    const RABBITMQ_URL = `amqp://${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`;
+    const connectRabbitMQ = new ConnectRabbitMQ();
+    try {
+      consumerChannel1 = await connectRabbitMQ.setRabbitMQUrl(RABBITMQ_URL).setQueueName(PRODUCER_QUEUE_NAME).connect();
+      consumerChannel2 = await connectRabbitMQ.setRabbitMQUrl(RABBITMQ_URL).setQueueName(CONSUMER_QUEUE_NAME).connect();
+      console.log("Consumer connected to RabbitMQ");
+      initiateConsumingMessages();
+    } catch (error) {
+      console.error('Error connecting to RabbitMQ:', error);
+      process.exit(1);
+    }
   });
 })()
--- producer/app.js
+++ producer/app.js
@@ -3,20 +3,41 @@ import ConnectRabbitMQ from './ConnectRabbitMQ.mjs';
 
 (() => {
   const PRODUCER_QUEUE_NAME = 'producer_messages';
-
+  const CONSUMER_QUEUE_NAME = 'consumer_messages';
   const app = express();
   app.use(express.json());
   const PORT = process.env.PORT || 3000;
-  let channel = null;
+  let producerChannel1 = null; // will be used for sending message to consumer
+  let producerChannel2 = null; // will be used for receiving message from consumer
+
+  const initiateConsumingMessages = async () => {
+    if (!producerChannel2) throw new Error('Consuming message queue is not connected to RabbitMQ');
+    try {
+      producerChannel2.consume(PRODUCER_QUEUE_NAME, async (message) => {
+        if (message) {
+          try {
+            const content = JSON.parse(message.content.toString());
+            console.log(`Received message from consumer: ${content.text} at ${content.timestamp}`);
+            await producerChannel2.ack(message);
+          } catch (error) {
+            console.error('Error processing message:', error);
+            await producerChannel2.nack(message);
+          }
+        }
+      });
+    } catch (error) {
+      console.error('Error consuming messages:', error);
+    }
+  };
 
   app.post('/producer/send', async (req, res) => {
     try {
-      if (!channel) throw new Error('Not connected to RabbitMQ');
+      if (!producerChannel1) throw new Error('Not connected to RabbitMQ');
       const message = {
         text: req.body.text || 'Hello from producer!',
         timestamp: new Date().toISOString()
       };
-      await channel.sendToQueue(PRODUCER_QUEUE_NAME, Buffer.from(JSON.stringify(message)));
+      await producerChannel1.sendToQueue(PRODUCER_QUEUE_NAME, Buffer.from(JSON.stringify(message)));
       res.status(201).send((JSON.stringify({ data: 'Message sent' })));
     } catch (error) {
       console.error('Error sending message:', error);
@@ -29,8 +50,10 @@ import ConnectRabbitMQ from './ConnectRabbitMQ.mjs';
     const RABBITMQ_URL = `amqp://${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`;
     const connectRabbitMQ = new ConnectRabbitMQ();
     try {
-      channel = await connectRabbitMQ.setRabbitMQUrl(RABBITMQ_URL).setQueueName(PRODUCER_QUEUE_NAME).connect();
+      producerChannel1 = await connectRabbitMQ.setRabbitMQUrl(RABBITMQ_URL).setQueueName(PRODUCER_QUEUE_NAME).connect();
+      producerChannel2 = await connectRabbitMQ.setRabbitMQUrl(RABBITMQ_URL).setQueueName(CONSUMER_QUEUE_NAME).connect();
       console.log("Producer connected to RabbitMQ");
+      initiateConsumingMessages();
     } catch (error) {
       console.error('Error connecting to RabbitMQ:', error);
       process.exit(1);
